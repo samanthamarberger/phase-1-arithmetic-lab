a46e729851f1907915750d7df6d64b1da2af3c8a
samanthamarberger <slatham04@gmail.com>
2022-09-19 12:41:09 -0600

    commit tree db9600361aae1f4ec6a7bf4789e1bdd14b71b0b9

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+console.log(multiply); 
+
+let random = Math.ceil(Math.random() * 10) + 1;
+console.log(random);
+
+let num3 = 9;
+let num4 = 5;
+let mod  = num3 % num4;
+
+let max = Math.max(5, 8, 2, 20);
